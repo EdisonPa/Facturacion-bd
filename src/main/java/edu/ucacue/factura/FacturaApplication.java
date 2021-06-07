5a27865f76785c5457bb6a6be2537f11e83afb22
ASUS <ASUS@DESKTOP-QJ59L4N>
2021-06-07 10:28:44 +0200

    Se implementa Ventanas internas y se crea un mantenimiento completo de modificar persona

--- src/main/java/edu/ucacue/factura/FacturaApplication.java
+++ src/main/java/edu/ucacue/factura/FacturaApplication.java
@@ -2,11 +2,13 @@ package edu.ucacue.factura;
 
 //import org.springframework.boot.SpringApplication;
 import  edu.ucacue.factura.controlador.PrincipalGui;
+import edu.ucacue.factura.controlador.Index;
 
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+
 
 import edu.ucacue.factura.controlador.Principal;
 
@@ -31,8 +33,11 @@ public class FacturaApplication {
 			    .web(WebApplicationType.NONE)
 			    .run(args);
 
-				PrincipalGui principaGui= contexto.getBean(PrincipalGui.class);
-				principaGui.setVisible(true);
+				/*PrincipalGui principaGui= contexto.getBean(PrincipalGui.class);
+				principaGui.setVisible(true);*/
+				
+				Index index= contexto.getBean(Index.class);
+				index.setVisible(true);
 	}
 
 }
--- src/main/java/edu/ucacue/factura/controlador/PrincipalGui.java
+++ src/main/java/edu/ucacue/factura/controlador/PrincipalGui.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 
 import edu.ucacue.factura.infraestructura.repositorio.PersonaRepository;
 import edu.ucacue.factura.modelo.Persona;
+import javax.swing.JInternalFrame;
 
 import javax.swing.JTextField;
 import javax.swing.JLabel;
@@ -24,7 +25,8 @@ import java.awt.event.MouseMotionAdapter;
 import java.awt.event.MouseEvent;
 
 @Controller
-public class PrincipalGui extends JFrame {
+//public class PrincipalGui extends JFrame {
+	public class PrincipalGui extends JInternalFrame {
 
 	private JPanel contentPane;
 	private JTextField txtNombre;
@@ -40,91 +42,91 @@ public class PrincipalGui extends JFrame {
 
 
 	public PrincipalGui() {
-		setTitle("Ventana Principal");
-		setBackground(Color.LIGHT_GRAY);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(0, 0, 398, 203);
-
-
-		contentPane = new JPanel();
-		contentPane.addMouseMotionListener(new MouseMotionAdapter() {
-			@Override
-			public void mouseMoved(MouseEvent e) {
-				System.out.println("X: "+e.getX()+"Y:"+e.getY());
-			}
-		});
-		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-
-		JLabel lblNewLabel = new JLabel("Nombre:");
-		lblNewLabel.setBounds(76, 8, 41, 14);
-		contentPane.add(lblNewLabel);
-
-		txtNombre = new JTextField();
-		txtNombre.setBounds(148, 5, 86, 20);
-		contentPane.add(txtNombre);
-		txtNombre.setColumns(10);
-
-		JLabel lblNewLabel_1 = new JLabel("Apellido:");
-		lblNewLabel_1.setBounds(76, 33, 41, 14);
-		contentPane.add(lblNewLabel_1);
-
-		txtApellido = new JTextField();
-		txtApellido.setBounds(148, 30, 86, 20);
-		txtApellido.setText("");
-		contentPane.add(txtApellido);
-		txtApellido.setColumns(10);
-
-		JLabel lblNewLabel_2 = new JLabel("Teléfono:");
-		lblNewLabel_2.setBounds(73, 58, 46, 14);
-		contentPane.add(lblNewLabel_2);
-
-		txtTelefono = new JTextField();
-		txtTelefono.setBounds(148, 55, 86, 20);
-		contentPane.add(txtTelefono);
-		txtTelefono.setColumns(10);
-
-		JLabel lblNewLabel_3 = new JLabel("Cédula");
-		lblNewLabel_3.setBounds(80, 83, 33, 14);
-		contentPane.add(lblNewLabel_3);
-
-		txtCedula = new JTextField();
-		txtCedula.setBounds(148, 80, 86, 20);
-		contentPane.add(txtCedula);
-		txtCedula.setColumns(10);
-
-		btnGuardar = new JButton("Guardar");
-		btnGuardar.setBounds(163, 119, 71, 23);
-		btnGuardar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-			String nombre = txtNombre.getText();
-			String apellido = txtApellido.getText();
-			String telefono = txtTelefono.getText();
-			String cedula = txtCedula.getText();
-
-			Persona p = new Persona(nombre, apellido, telefono, cedula);
-			System.out.println(p);
-			guardarPersona(p);
-
-			}
-		});
-		contentPane.add(btnGuardar);
+		
+			setTitle("Nueva Persona");
+			setBackground(Color.LIGHT_GRAY);
+			//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setBounds(0, 0, 384, 180);
+			
+			this.setMaximizable(true); // maximize
+			this.setIconifiable(true); // set minimize
+			this.setClosable(true); // set closed
+			this.setResizable(true); // set resizable
+			
+			
+			contentPane = new JPanel();
+			contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+			setContentPane(contentPane);
+			contentPane.setLayout(null);
+			
+			JLabel lblNewLabel = new JLabel("Nombre:");
+			lblNewLabel.setBounds(75, 8, 41, 14);
+			contentPane.add(lblNewLabel);
+			
+			txtNombre = new JTextField();
+			txtNombre.setBounds(176, 5, 86, 20);
+			contentPane.add(txtNombre);
+			txtNombre.setColumns(10);
+			
+			JLabel lblNewLabel_1 = new JLabel("Apellido:");
+			lblNewLabel_1.setBounds(77, 33, 47, 14);
+			contentPane.add(lblNewLabel_1);
+			
+			txtApellido = new JTextField();
+			txtApellido.setBounds(176, 30, 86, 20);
+			txtApellido.setText("");
+			contentPane.add(txtApellido);
+			txtApellido.setColumns(10);
+			
+			JLabel lblNewLabel_2 = new JLabel("Teléfono:");
+			lblNewLabel_2.setBounds(75, 58, 49, 14);
+			contentPane.add(lblNewLabel_2);
+			
+			txtTelefono = new JTextField();
+			txtTelefono.setBounds(176, 55, 86, 20);
+			contentPane.add(txtTelefono);
+			txtTelefono.setColumns(10);
+			
+			JLabel lblNewLabel_3 = new JLabel("Cédula:");
+			lblNewLabel_3.setBounds(80, 83, 41, 14);
+			contentPane.add(lblNewLabel_3);
+			
+			txtCedula = new JTextField();
+			txtCedula.setBounds(176, 80, 86, 20);
+			contentPane.add(txtCedula);
+			txtCedula.setColumns(10);
+			
+			btnGuardar = new JButton("Guardar");
+			btnGuardar.setBounds(148, 105, 71, 23);
+			btnGuardar.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					
+				String nombre = txtNombre.getText();
+				String apellido = txtApellido.getText();
+				String telefono = txtTelefono.getText();
+				String cedula = txtCedula.getText();
+				
+				Persona p = new Persona(nombre, apellido, telefono, cedula);
+				System.out.println(p);
+				guardarPersona(p);
+				
+				}
+			});
+			contentPane.add(btnGuardar);
+		}
+		
+		public void borrarDatos()
+		{
+			txtNombre.setText("");
+			txtApellido.setText("");
+			txtTelefono.setText("");
+			txtCedula.setText("");
+		}
+		
+		
+		public void guardarPersona(Persona p)
+		{
+			personaRepositorio.save(p);
+			borrarDatos();
+		}
 	}
-
-	public void borrarDatos()
-	{
-		txtNombre.setText("");
-		txtApellido.setText("");
-		txtTelefono.setText("");
-		txtCedula.setText("");
-	}
-
-
-	public void guardarPersona(Persona p)
-	{
-		personaRepositorio.save(p);
-		borrarDatos();
-	}
-}
